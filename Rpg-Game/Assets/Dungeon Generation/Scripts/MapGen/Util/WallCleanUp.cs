1a2470d63b04be5ec4c1caf4894981e4c2201fc6
Watevz <rorywelsh5@gmail.com>
2019-07-22 18:20:55 +0200

    update to generation (closed walls clean up)
    - added code to closed room prefab to delete walls that don't border a floor

---
+++ Rpg-Game/Assets/Dungeon Generation/Scripts/MapGen/Util/WallCleanUp.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapGen
+{
+    public class WallCleanUp : MonoBehaviour
+    {
+        private float timer = 6f;
+        private bool wallBordersRoom = false;
+
+        /// <summary>
+        /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        void FixedUpdate()
+        {
+            if(timer <= 0 && !wallBordersRoom){
+                Destroy(gameObject);
+            }
+            else
+            {
+                timer--;
+            }
+            
+        }
+
+        /// <summary>
+        /// OnTriggerEnter is called when the Collider other enters the trigger.
+        /// </summary>
+        /// <param name="other">The other Collider involved in this collision.</param>
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Floor"))
+            {
+                wallBordersRoom = true;
+                Destroy(this);
+            }
+
+        }
+    }
+    
+}
+
+
