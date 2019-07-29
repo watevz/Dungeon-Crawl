f74236235bb25dfcb7d3f4315cc0497d211d7185
Watevz <rorywelsh5@gmail.com>
2019-07-29 12:56:43 +0200

    updated player move and camera scripts to old rpg ones

--- Rpg-Game/Assets/Player-Roguelike/Scripts/PlayerController.cs
+++ Rpg-Game/Assets/Player-Roguelike/Scripts/PlayerController.cs
@@ -1,79 +1,100 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+[RequireComponent(typeof(PlayerMovement))]
 
+public class PlayerController : MonoBehaviour {
 
-namespace Player
-{
-    [RequireComponent(typeof(PlayerMovement))]
-    [RequireComponent(typeof(Rigidbody))]
-    public class PlayerController : MonoBehaviour {
+    public LayerMask groundLayer;
+    private PlayerMovement motor;
+    private int floorMask;
+    //private CharacterAnimator animator;
 
-        public LayerMask groundLayer;
-        private PlayerMovement motor;
-        private Rigidbody playerRigidbody;
-        float camRayLength = 100f;
-        //private CharacterAnimator animator;
+    // public Interactable focus;
 
-        private void Start()
-        {
-            playerRigidbody = GetComponent<Rigidbody>();
-            motor = GetComponent<PlayerMovement>();
-            //animator = GetComponent<CharacterAnimator>();
-        }
-
-        private void FixedUpdate()
-        {
-            MouseActivity();
-            KeyActivity();
-            //animator.StateCheck(focus);
-        }
-
-        private void MouseActivity()
-        {
-            Turning();
-            if (Input.GetMouseButton(0))
-            {
-                LeftMouseClick();
-            }
-            if (Input.GetMouseButton(1))
-            {
-                RightMouseClick();
-            }
-        }
-
-        private void LeftMouseClick()
-        {
-
-        }
-
-        private void RightMouseClick()
-        {
-
-        }
-
-        private void KeyActivity()
-        {
-            // Store the input axes.
-            float x = Input.GetAxisRaw ("Horizontal");
-            float z = Input.GetAxisRaw ("Vertical");
-
-            motor.Move(x,z);
-        }
-
-        private void Turning ()
-        {
-            Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-            RaycastHit floorHit;
-
-            if(Physics.Raycast (camRay, out floorHit, camRayLength, groundLayer))
-            {
-                Vector3 playerToMouse = floorHit.point - transform.position;
-                playerToMouse.y = 0f;
-                Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
-                playerRigidbody.MoveRotation (newRotation);
-            }
-        }
-
+    private void Start()
+    {
+        motor = GetComponent<PlayerMovement>();
+        //animator = GetComponent<CharacterAnimator>();
     }
-}
 
+    private void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+        MouseActivity();
+        //animator.StateCheck(focus);
+    }
+
+    private void MouseActivity()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            LeftMouseClick();
+        }
+        if (Input.GetMouseButton(1))
+        {
+            RightMouseClick();
+        }
+    }
+
+    private void LeftMouseClick()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, groundLayer))
+        {
+            // move to position
+            motor.Move(hit.point);
+            // stop focusing any objects
+            //RemoveFocus();
+        }
+    }
+
+    private void RightMouseClick()
+    {
+        //set focus to right clicked object
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        // if (Physics.Raycast(ray, out hit))
+        // {
+        //     Interactable interactable = hit.collider.GetComponent<Interactable>();
+        //     if (interactable != null)
+        //     {
+        //         SetFocus(interactable);
+        //     }
+        // }
+    }
+
+    // private void SetFocus(Interactable targetInteractable)
+    // {
+    //     if(focus != targetInteractable)
+    //     {
+    //         if( focus != null)
+    //         {
+    //             focus.DeFocused();
+    //         }
+            
+    //         focus = targetInteractable;
+    //         motor.FollowTarget(focus);
+            
+    //     }
+
+    //     targetInteractable.OnFocused(transform);
+    //     animator.StateCheck(focus);
+    // }
+
+    // private void RemoveFocus()
+    // {
+    //     if (focus != null)
+    //     {
+    //         focus.DeFocused();
+    //     }
+
+    //     focus = null;
+    //     motor.StopFollowingTarget();
+    //     animator.StateCheck(focus);
+    // }
+}
--- Rpg-Game/Assets/Player-Roguelike/Scripts/PlayerMovement.cs
+++ Rpg-Game/Assets/Player-Roguelike/Scripts/PlayerMovement.cs
@@ -1,25 +1,58 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.AI;
 
-namespace Player
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMovement : MonoBehaviour
 {
-    public class PlayerMovement : MonoBehaviour
+    private NavMeshAgent agent;
+    public float moveSpeed = 6f;
+    public float angSpeed = 720;
+    public float accelerationSpeed = 10;
+
+    public Transform target;
+   
+
+    void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = moveSpeed;
+        agent.angularSpeed = angSpeed;
+        agent.acceleration = accelerationSpeed;
+    }
 
-        public float speed = 10;
-        private Vector3 movement;  
-        private Rigidbody playerRigidbody;
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
 
-        void Start()
-        {
-            playerRigidbody = GetComponent<Rigidbody>();
-        }
-        public void Move (float x, float z)
-        {
-            movement.Set (x, 0f, z);
-            movement = movement.normalized * speed * Time.deltaTime;
-            playerRigidbody.MovePosition (transform.position + movement);
-        }
-    }   
+    public void Move(Vector3 targetPosition)
+    {
+        agent.SetDestination(targetPosition);
+    }
+
+    // public void FollowTarget(Interactable targetToFollow)
+    // {
+    //     target = targetToFollow.interactionZone;
+    //     agent.stoppingDistance = targetToFollow.interactionRadius * .8f;
+    //     agent.updateRotation = false;
+    // }
+
+    public void StopFollowingTarget()
+    {
+        target = null;
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true;
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
+    }
 }
-
